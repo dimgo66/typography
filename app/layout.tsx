38b2c63d5491a7335aedaaa402ed44b5fead505a
dimgo66 <2dimgo@gmail.com>
2025-06-11 03:04:50 +0300

    feat: добавлен SVG favicon в <head> для современных браузеров

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,6 +12,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ru">
+      <head>
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+      </head>
       <body className="font-sans antialiased">{children}</body>
     </html>
   )
